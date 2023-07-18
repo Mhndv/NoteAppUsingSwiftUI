0af8738f3069770e5bb4565ce4e1edf57c69a8af
Mohanned Alsahaf <mtkd2000@hotmail.com>
2023-07-18 19:31:08 +0300

    Initial Commit

---
+++ NoteAppUsingSwiftUI/NoteAppUsingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppUsingSwiftUIApp.swift
+//  NoteAppUsingSwiftUI
+//
+//  Created by Mohanned Alsahaf on 30/12/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppUsingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
